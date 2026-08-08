95956499764bf8a45abfb0168b0f4fecc8bf7de4
JEAN-TC <jeantoscano5@gmail.com>
2026-08-08 13:21:52 -0500

    Añade entradas suaves al recorrer el inicio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,6 @@
+Exit code: 0
+Wall time: 2.3 seconds
+Output:
 <script setup lang="ts">
 import Hero from '../components/Hero.vue'
 import About from '../components/About.vue'
@@ -12,12 +15,44 @@ import Contact from '../components/Contact.vue'
 <template>
   <main class="w-full relative z-10">
     <Hero />
-    <About />
-    <Skills />
-    <Certifications />
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="1100" data-aos-offset="120">
+      <About />
+    </div>
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="1050" data-aos-offset="120">
+      <Skills />
+    </div>
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="1050" data-aos-offset="120">
+      <Certifications />
+    </div>
+
     <Projects />
-    <Services />
-    <Contact />
-    <Stats />
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="110">
+      <Services />
+    </div>
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="950" data-aos-offset="100">
+      <Contact />
+    </div>
+
+    <div class="scroll-reveal" data-aos="fade-up" data-aos-duration="900" data-aos-offset="90">
+      <Stats />
+    </div>
   </main>
 </template>
+
+<style scoped>
+.scroll-reveal {
+  will-change: opacity, transform;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .scroll-reveal {
+    will-change: auto;
+  }
+}
+</style>
+
